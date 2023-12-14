9d7e629ea180e0f84f6cfa32aa60fa52e0daffe4
ghislain kabangu <kabangughislain77@gmail.com>
2023-12-14 22:19:44 +0100

    commit

--- js/index.js
+++ js/index.js
@@ -1,44 +1,12 @@
 /** @format */
 
-let listClient = [];
-
-let form = document.querySelector("form");
-
-form.addEventListener("submit", (envoie) => {
-  envoie.preventDefault();
-  let nom = document.getElementById("nom").value;
-  let postnom = document.getElementById("postnom").value;
+let formulaire = document.querySelector("form");
+formulaire.addEventListener("submit", () => {
   let prenom = document.getElementById("prenom").value;
- 
-  let numTelephone = document.getElementById("tel").value;
-  let courier = document.getElementById("mail").value;
 
-  let communeClient;
-  let commune = document.getElementById("commune");
-  communeClient = commune.options[commune.selectedIndex].value;
-
-  let quatier = document.getElementById("quartier").value;
-  let rue = document.getElementById("rue").value;
-
-  let choixAticle;
-  let article = document.getElementById("article");
-  choixAticle = article.options[article.selectedIndex].value;
-  let nombreArticle = document.getElementById("nombre").value;
-  let messageClient=document.querySelector("textarea").value
-
-  let infoClient = {
-    nom,
-    postnom,
-    prenom,
-    numTelephone,
-    courier,
-    communeClient,
-    quatier,
-    rue,
-    choixAticle,
-    nombreArticle,
-    messageClient
-  };
-  listClient.push(infoClient);
-  console.log(listClient);
+  alert(
+    "merci " +
+      prenom +
+      " votre commande a été bien reҫue , \n nous sommes fiers de vous compter parmis nos clients .\n  Notre équipe vous contactera dans les prochaines heures"
+  );
 });
--- js/script.js
+++ js/script.js
@@ -1,18 +1,44 @@
 /** @format */
+// CODE DE GESTION DU MENU
+let sidenav = document.getElementsByClassName("sidenav")[0];
+let openBtn = document.getElementById("openBtn");
+let closeBtn = document.getElementById("closeBtn");
 
-let prix = document.querySelectorAll("h5");
+openBtn.addEventListener("click", () => {
+  sidenav.style = "left: -0px";
+});
 
-let article = document.querySelectorAll("article");
+closeBtn.addEventListener("click", () => {
+  sidenav.style = "left: -40%";
+});
+
+//CODE DE GESTION DES INTERACTIONS DES ARTICLES
+
+let article = document.querySelectorAll("article"); // c'est tableau contenant les articles
 for (let i = 0; i < article.length; i++) {
+  // on parcourt le tableau
   article[i].addEventListener("mouseover", () => {
+    /* au passage de souris sur un article [y]on attribue cet artcle une nouvelle class contenant des nouvelles propriétes CSS */
     article[i].classList.add("article");
-     
-    article[i].title="Article Vente"
-    
+
+    article[i].title = "Vous êtes prêts à acheter cet article ";
   });
 
   article[i].addEventListener("mouseout", () => {
+    /*lorsque la souris sort de l'article[y] on supprime qui lui avait été attribué*/
     article[i].classList.remove("article");
   });
 }
 
+// BARRE DE PROGRESSION
+
+let progressbar = document.querySelectorAll(".progressionbar");
+for (let x = 0; x < progressbar.length; x++) {
+  progressbar[x].addEventListener("mouseover", () => {
+    progressbar[x].classList.add("progressbarInjs");
+  });
+
+  progressbar[x].addEventListener("mouseout", () => {
+    progressbar[x].classList.remove("progressbarInjs");
+  });
+}
